2ecd8cde52077d65f35017306d8fcf9506252f5b
HannahRichard13 <hannah13richard@gmail.com>
2025-04-23 13:57:23 -0400

    working

--- assignment_2.js
+++ assignment_2.js
@@ -1,11 +1,72 @@
 function populateQuote() {
-    const quote = document.getElementById('quote')
-    fetch('https://zenquotes.io/api/random')
-    .then((result) => result.json())
-    .then((resultJson) => {
-        console.log(resultJson);
-        quote.innerHTML = `"${resultJson[0].q}" - ${resultJson[0].a}`;
-        });
+    if (document.querySelector("#quote_box")) {
+        const quote = document.getElementById('quote')
+        fetch('https://zenquotes.io/api/random')
+        .then((result) => result.json())
+        .then((resultJson) => {
+            console.log(resultJson);
+            quote.innerHTML = `"${resultJson[0].q}" - ${resultJson[0].a}`;
+            });
+        } else {
+
+        }
     };
 
-window.onload = populateQuote;
+
+const commands = {
+
+'hello': () => { alert('Hello world!'); },
+
+'change the color to *color': (color) => {
+    document.body.style.backgroundColor = color;
+},
+
+'navigate to *page': (page) => {
+    if (page === 'home') {
+        window.location.href = 'home_page.html';
+    } else if (page === 'stocks') {
+        window.location.href = 'stocks_page.html';
+    } else if (page === 'dogs') {
+        window.location.href = 'dogs_page.html';
+    }}
+};
+
+
+function startListen() {
+    if (annyang) {
+        annyang.addCommands(commands);
+        annyang.setLanguage('en-US');
+        annyang.start();
+        console.log('started listening');
+        sessionStorage.setItem('isListening', 'true');
+
+    }
+}
+
+function stopListen() {
+    if (annyang) {
+        annyang.abort();
+        console.log('stopped listening');
+    }
+}
+
+
+function checkStorage() {
+
+
+    if (sessionStorage.getItem('isListening') === 'true') {
+        console.log('Listening is on')
+        annyang.start();
+    } else {
+        sessionStorage.getItem('isListening') === 'false' 
+        console.log('Listening is off')
+        annyang.abort();
+    }
+}
+
+
+
+window.onload = function() {
+    populateQuote();
+    checkStorage();
+};
